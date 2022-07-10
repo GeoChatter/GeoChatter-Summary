9b7237ddd33605373a78ecbc3b7e039bd65f354f
Sören Michaels <soeren.michaels@gmail.com>
2022-07-10 10:45:24 +0200

    created index.svelte

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	function routeToPage(route: string, replaceState: boolean) {
+		goto(`/results/${route}`, { replaceState });
+	}
+	let value = '';
+</script>
+
+<div class="flex justify-center items-centerh-screen">
+	<div>
+		<span class="text-white p-2">enter results id</span><input
+			class="input "
+			type="text"
+			bind:value
+		/>
+		<button
+			class="btn"
+			on:click={() => {
+				routeToPage(value, true);
+			}}>go</button
+		>
+	</div>
+</div>
