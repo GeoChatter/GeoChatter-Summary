4072156ba4b3f688d7fbde44c3cdae332bc6e99f
Sören Michaels <soeren.michaels@gmail.com>
2022-07-10 10:59:35 +0200

    changed to query

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,55 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	function routeToPage(route: string, replaceState: boolean) {
-		goto(`/results/${route}`, { replaceState });
+	import * as signalR from '@microsoft/signalr';
+	import Streetview from '../lib/components/Streetview.svelte';
+	import Map from '../lib/components/Map.svelte';
+	import Scoreboard from '../lib/components/Scoreboard.svelte';
+
+	import type { Response } from '../types/Game';
+	import { browser } from '$app/env';
+
+	let pano: Response.RoundLocation;
+	import { page } from '$app/stores';
+	const id = $page.url.searchParams.get('id');
+	const connection = new signalR.HubConnectionBuilder()
+		.withUrl('https://dev.geochatter.tv/guess/geoChatterHub')
+		.build();
+	const startRes = connection.start();
+	const getGameSummary = async (gameId: string) => {
+		await startRes;
+		const res: Response.Game = await connection.invoke('GetSummary', gameId);
+
+		console.log(res);
+		return res;
+	};
+	let gameRes: Promise<Response.Game>;
+	if (id) {
+		gameRes = getGameSummary(id);
 	}
-	let value = '';
+
+	//
 </script>
 
-<div class="flex justify-center items-centerh-screen">
-	<div>
-		<span class="text-white p-2">enter results id</span><input
-			class="input "
-			type="text"
-			bind:value
-		/>
-		<button
-			class="btn"
-			on:click={() => {
-				routeToPage(value, true);
-			}}>go</button
-		>
+<div class="">
+	<div class="navbar rounded bg-base-100 w-full">
+		<a href="https://geochatter.tv" class="btn btn-ghost normal-case text-xl">GeoChatter.tv</a>
 	</div>
 </div>
+
+{#await gameRes then game}
+	<div class="flex flex-col  h-full space-y-2 pt-2">
+		<div class="relative w-full h-96 ">
+			<Map
+				callback={(p) => {
+					pano = p;
+					pano = pano;
+				}}
+				{game}
+			/>
+		</div>
+
+		<Streetview bind:pano />
+		<div>
+			<Scoreboard {game} />
+		</div>
+	</div>
+{/await}
